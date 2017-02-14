e15609a9bd8f4289a618d403e4e19fea3092fa83
miguel robles <roblesm899@gmail.com>
2017-02-14 13:32:00 -0800

    DONE

---
+++ random.js
@@ -0,0 +1,368 @@
+var namespace = "http://www.w3.org/2000/svg"
+
+
+// Fill in this function so that it draws something using SVG shapes!
+// You need to use at least 3 different types of shape.
+// Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
+function createFirstScene() {
+makeRect(3,25, 200, 200, "dodgerblue", 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 2, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 10, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 20, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 30, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 40, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 50, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 60, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 70, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 80, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 90, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 100, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 110, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 120, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 130, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 140, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 150, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 160, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 170, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 180, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 190, 90, 10, 10, 1)
+makeImage("https://ih0.redbubble.net/image.23778914.0662/sticker,375x360.u4.png", 55, 60, 9, 9, 1)
+makeImage("http://gallery2.jpmullan.com/d/443036-2/castle.png", 172, 60,30 , 30, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 63, 60, 8, 8, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 48, 60, 8, 8, 1)
+makeImage("https://img.fireden.net/v/image/1470/68/1470683831264.png", 10, 74, 16,16 , 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 100, 60, 8, 8, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 115, 60, 8, 8, 1)
+makeImage("https://ih0.redbubble.net/image.23778914.0662/sticker,375x360.u4.png", 108, 60, 9, 9, 1)
+makeImage("http://vignette3.wikia.nocookie.net/fantendo/images/2/26/Flagpole.png/revision/latest?cb=20111001093313", 145, 40, 50, 50, 1)
+makeCircle(40, 40, 5, "white", 1)
+makeCircle(45, 40, 5, "white", 1)
+makeCircle(40, 45, 5, "white", 1)
+makeCircle(35, 40, 5, "white", 1)
+makeCircle(40, 35, 5, "white", 1)
+makeCircle(45, 35, 5, "white", 1)
+makeCircle(35, 45, 5, "white", 1)
+
+makeCircle(75, 40, 5, "white", 1)
+makeCircle(80, 40, 5, "white", 1)
+makeCircle(75, 45, 5, "white", 1)
+makeCircle(70, 40, 5, "white", 1)
+makeCircle(75, 35, 5, "white", 1)
+makeCircle(80, 35, 5, "white", 1)
+makeCircle(70, 45, 5, "white", 1)
+
+makeCircle(105, 40, 5, "white", 1)
+makeCircle(110, 40, 5, "white", 1)
+makeCircle(105, 45, 5, "white", 1)
+makeCircle(100, 40, 5, "white", 1)
+makeCircle(105, 35, 5, "white", 1)
+makeCircle(110, 35, 5, "white", 1)
+makeCircle(100, 45, 5, "white", 1)
+
+makeCircle(135, 40, 5, "white", 1)
+makeCircle(140, 40, 5, "white", 1)
+makeCircle(135, 45, 5, "white", 1)
+makeCircle(130, 40, 5, "white", 1)
+makeCircle(135, 35, 5, "white", 1)
+makeCircle(140, 35, 5, "white", 1)
+makeCircle(130, 45, 5, "white", 1)
+
+makeCircle(165, 40, 5, "white", 1)
+makeCircle(170, 40, 5, "white", 1)
+makeCircle(165, 45, 5, "white", 1)
+makeCircle(160, 40, 5, "white", 1)
+makeCircle(165, 35, 5, "white", 1)
+makeCircle(170, 35, 5, "white", 1)
+makeCircle(160, 45, 5, "white", 1)
+
+}
+
+// Fill in this function so that it draws something using SVG shapes!
+// You need to use at least 3 different types of shape.
+// Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
+function createSecondScene() {
+ makeRect(3,25, 200, 200, "dodgerblue", 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 2, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 10, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 20, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 30, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 40, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 50, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 60, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 70, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 80, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 90, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 100, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 110, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 120, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 130, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 140, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 150, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 160, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 170, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 180, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 190, 90, 10, 10, 1)
+makeImage("https://ih0.redbubble.net/image.23778914.0662/sticker,375x360.u4.png", 55, 60, 9, 9, 1)
+makeImage("http://gallery2.jpmullan.com/d/443036-2/castle.png", 172, 60,30 , 30, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 63, 60, 8, 8, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 48, 60, 8, 8, 1)
+makeImage("https://img.fireden.net/v/image/1470/68/1470683831264.png", 105, 68, 16,16 , 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 100, 60, 8, 8, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 115, 60, 8, 8, 1)
+makeImage("https://ih0.redbubble.net/image.23778914.0662/sticker,375x360.u4.png", 108, 60, 9, 9, 1)
+makeImage("http://vignette3.wikia.nocookie.net/fantendo/images/2/26/Flagpole.png/revision/latest?cb=20111001093313", 145, 40, 50, 50, 1)
+makeCircle(40, 40, 5, "white", 1)
+makeCircle(45, 40, 5, "white", 1)
+makeCircle(40, 45, 5, "white", 1)
+makeCircle(35, 40, 5, "white", 1)
+makeCircle(40, 35, 5, "white", 1)
+makeCircle(45, 35, 5, "white", 1)
+makeCircle(35, 45, 5, "white", 1)
+
+makeCircle(75, 40, 5, "white", 1)
+makeCircle(80, 40, 5, "white", 1)
+makeCircle(75, 45, 5, "white", 1)
+makeCircle(70, 40, 5, "white", 1)
+makeCircle(75, 35, 5, "white", 1)
+makeCircle(80, 35, 5, "white", 1)
+makeCircle(70, 45, 5, "white", 1)
+
+makeCircle(105, 40, 5, "white", 1)
+makeCircle(110, 40, 5, "white", 1)
+makeCircle(105, 45, 5, "white", 1)
+makeCircle(100, 40, 5, "white", 1)
+makeCircle(105, 35, 5, "white", 1)
+makeCircle(110, 35, 5, "white", 1)
+makeCircle(100, 45, 5, "white", 1)
+
+makeCircle(135, 40, 5, "white", 1)
+makeCircle(140, 40, 5, "white", 1)
+makeCircle(135, 45, 5, "white", 1)
+makeCircle(130, 40, 5, "white", 1)
+makeCircle(135, 35, 5, "white", 1)
+makeCircle(140, 35, 5, "white", 1)
+makeCircle(130, 45, 5, "white", 1)
+
+makeCircle(165, 40, 5, "white", 1)
+makeCircle(170, 40, 5, "white", 1)
+makeCircle(165, 45, 5, "white", 1)
+makeCircle(160, 40, 5, "white", 1)
+makeCircle(165, 35, 5, "white", 1)
+makeCircle(170, 35, 5, "white", 1)
+makeCircle(160, 45, 5, "white", 1)   
+}
+
+
+// Fill in this function so that it draws something using SVG shapes!
+// You need to use at least 3 different types of shape.
+// Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
+function createThirdScene() {
+  makeRect(3,25, 200, 200, "dodgerblue", 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 2, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 10, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 20, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 30, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 40, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 50, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 60, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 70, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 80, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 90, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 100, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 110, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 120, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 130, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 140, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 150, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 160, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 170, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 180, 90, 10, 10, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 190, 90, 10, 10, 1)
+makeImage("https://ih0.redbubble.net/image.23778914.0662/sticker,375x360.u4.png", 55, 60, 9, 9, 1)
+makeImage("http://gallery2.jpmullan.com/d/443036-2/castle.png", 172, 60,30 , 30, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 63, 60, 8, 8, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 48, 60, 8, 8, 1)
+makeImage("https://img.fireden.net/v/image/1470/68/1470683831264.png", 170, 74, 16,16 , 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 100, 60, 8, 8, 1)
+makeImage("http://makepixelart.com/peoplepods/files/images/1323542.original.png", 115, 60, 8, 8, 1)
+makeImage("https://ih0.redbubble.net/image.23778914.0662/sticker,375x360.u4.png", 108, 60, 9, 9, 1)
+makeImage("http://vignette3.wikia.nocookie.net/fantendo/images/2/26/Flagpole.png/revision/latest?cb=20111001093313", 145, 40, 50, 50, 1)
+makeCircle(40, 40, 5, "white", 1)
+makeCircle(45, 40, 5, "white", 1)
+makeCircle(40, 45, 5, "white", 1)
+makeCircle(35, 40, 5, "white", 1)
+makeCircle(40, 35, 5, "white", 1)
+makeCircle(45, 35, 5, "white", 1)
+makeCircle(35, 45, 5, "white", 1)
+
+makeCircle(75, 40, 5, "white", 1)
+makeCircle(80, 40, 5, "white", 1)
+makeCircle(75, 45, 5, "white", 1)
+makeCircle(70, 40, 5, "white", 1)
+makeCircle(75, 35, 5, "white", 1)
+makeCircle(80, 35, 5, "white", 1)
+makeCircle(70, 45, 5, "white", 1)
+
+makeCircle(105, 40, 5, "white", 1)
+makeCircle(110, 40, 5, "white", 1)
+makeCircle(105, 45, 5, "white", 1)
+makeCircle(100, 40, 5, "white", 1)
+makeCircle(105, 35, 5, "white", 1)
+makeCircle(110, 35, 5, "white", 1)
+makeCircle(100, 45, 5, "white", 1)
+
+makeCircle(135, 40, 5, "white", 1)
+makeCircle(140, 40, 5, "white", 1)
+makeCircle(135, 45, 5, "white", 1)
+makeCircle(130, 40, 5, "white", 1)
+makeCircle(135, 35, 5, "white", 1)
+makeCircle(140, 35, 5, "white", 1)
+makeCircle(130, 45, 5, "white", 1)
+
+makeCircle(165, 40, 5, "white", 1)
+makeCircle(170, 40, 5, "white", 1)
+makeCircle(165, 45, 5, "white", 1)
+makeCircle(160, 40, 5, "white", 1)
+makeCircle(165, 35, 5, "white", 1)
+makeCircle(170, 35, 5, "white", 1)
+makeCircle(160, 45, 5, "white", 1)  
+}
+
+
+
+
+
+// FILL IN THIS FUNCTION!
+// This function is called whenever you press the "Go!" button.
+function createRandomScene() {
+    // Generate a random number between 0 and 1, and store it in a variable.
+    var bob = Math.random()
+    // If the number is less than 0.33, call the function to create your first scene.
+    if (bob < 0.33){
+        createFirstScene()
+    }
+    
+    // Else, if the number is less than 0.67, call the function to create your second scene.
+    else if(bob < 0.67){ 
+       createSecondScene() 
+    }
+    
+    // Else, call the function to create your third scene.
+    else{
+        createThirdScene()
+    }
+}
+    //-0i8// DO NOT EDIT ANYTHING BELOW THIS LINE!
+// These are the functions you should call to
+// easily create shapes in JavaScript. Feel free
+// to check them out if you're curious how they
+// work, but don't change them!
+// <3 Ms. Squires
+function makeCircle(cx, cy, r, fill, opacity) {
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", cx)
+  circle.setAttribute("cy", cy)
+  circle.setAttribute("r", r)
+  circle.setAttribute("fill", fill)
+  circle.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(circle)
+  return circle
+}
+
+function makeRect(x, y, width, height, fill, opacity) {
+  var rect = document.createElementNS(namespace, "rect")
+  rect.setAttribute("x", x)
+  rect.setAttribute("y", y)
+  rect.setAttribute("width", width)
+  rect.setAttribute("height", height)
+  rect.setAttribute("fill", fill)
+  rect.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(rect)
+  return rect
+}
+
+function makeEllipse(cx, cy, rx, ry, fill, opacity) {
+  var ellipse = document.createElementNS(namespace, "ellipse")
+  ellipse.setAttribute("cx", cx)
+  ellipse.setAttribute("cy", cy)
+  ellipse.setAttribute("rx", rx)
+  ellipse.setAttribute("ry", ry)
+  ellipse.setAttribute("fill", fill)
+  ellipse.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(ellipse)
+  return ellipse
+}
+
+function makeLine(x1, y1, x2, y2, stroke, strokeWidth, opacity) {
+  var line = document.createElementNS(namespace, "line")
+  line.setAttribute("x1", x1)
+  line.setAttribute("y1", y1)
+  line.setAttribute("x2", x2)
+  line.setAttribute("y2", y2)
+  line.setAttribute("stroke", stroke)
+  line.setAttribute("stroke-width", strokeWidth)
+  line.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(line)
+  return line
+}
+
+function makePolyline(points, stroke, strokeWidth, opacity) {
+  var polyline = document.createElementNS(namespace, "polyline")
+  polyline.setAttribute("points", points)
+  polyline.setAttribute("stroke", stroke)
+  polyline.setAttribute("stroke-width", strokeWidth)
+  polyline.setAttribute("opacity", opacity)
+  polyline.setAttribute("fill", "none")
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(polyline)
+  return polyline
+}
+
+function makePolygon(points, fill, opacity) {
+  var polygon = document.createElementNS(namespace, "polygon")
+  polygon.setAttribute("points", points)
+  polygon.setAttribute("opacity", opacity)
+  polygon.setAttribute("fill", fill)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(polygon)
+  return polygon
+}
+
+function makeText(message, x, y, fontSize, fontFamily, fill, opacity) {
+  var text = document.createElementNS(namespace, "text")
+  text.innerHTML = message
+  text.setAttribute("x", x)
+  text.setAttribute("y", y)
+  text.setAttribute("font-size", fontSize)
+  text.setAttribute("font-family", fontFamily)
+  text.setAttribute("fill", fill)
+  text.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(text)
+  return text
+}
+
+function makeImage(url, x, y, width, height, opacity) {
+  var image = document.createElementNS(namespace, "image")
+  image.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", url)
+  image.setAttribute("x", x)
+  image.setAttribute("y", y)
+  image.setAttribute("width", width)
+  image.setAttribute("height", height)
+  image.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(image)
+  return image
+}
